3ee80edd3c69d6d80270fc4090e8649a81059f6a
Daniel Laing <daniel@daniellaing.com>
2026-08-24 09:34:03 +0100

    Use `nixpkgs-unstable`

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Document containing information about Bon Accord Free Church cleaning.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
